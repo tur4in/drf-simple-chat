b52fbbf9c99f8ef248fb1aa9c0048c3787938550
Andrii Turchyn <andrii@Andriis-MacBook-Air.local>
2024-04-05 02:15:52 +0300

    Threads logic adjustments and db gitigore cache removed

--- simple_chat/chat/api/views.py
+++ simple_chat/chat/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, mixins, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 from chat.models import Thread, Message
 from .serializers import ThreadSerializer, MessageSerializer
 from django.contrib.auth.models import User
@@ -41,15 +42,30 @@ class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
 
+    
     def get_queryset(self):
         thread_id = self.kwargs.get('thread_id')
         if thread_id:
+            thread = get_object_or_404(Thread, pk=thread_id)
+            
+            # Check if the request.user is a participant of the thread
+            if not thread.participants.filter(
+                pk=self.request.user.pk
+            ).exists():
+                raise PermissionDenied("You are not a participant of this thread.")
+                
             return self.queryset.filter(thread_id=thread_id)
-        return self.queryset
+        else:
+            # If no specific thread is requested, filter messages by user participation in any thread
+            user_threads = Thread.objects.filter(participants=self.request.user)
+            return self.queryset.filter(thread__in=user_threads)
 
     def perform_create(self, serializer):
         thread_id = self.kwargs.get('thread_id')
         thread = get_object_or_404(Thread, pk=thread_id)
+        # Check if the request.user is a participant of the thread
+        if not thread.participants.filter(pk=self.request.user.pk).exists():
+            raise PermissionDenied("You are not a participant of this thread.")
         serializer.save(sender=self.request.user, thread=thread)
 
     @action(detail=True, methods=['patch'])
